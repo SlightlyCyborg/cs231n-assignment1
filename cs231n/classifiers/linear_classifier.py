3973053cbb8cebf3a10753c4eacc1d16d0dc2275
slightlycyborg <collinalexbell@gmail.com>
2016-11-12 18:09:37 -0500

    softmax

--- cs231n/classifiers/linear_classifier.py
+++ cs231n/classifiers/linear_classifier.py
@@ -49,7 +49,13 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+
+      #Choose the indicies
+      indices = np.random.choice(num_train, batch_size)
+
+      #Create the batches
+      X_batch = X[indices]
+      y_batch = y[indices]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -63,7 +69,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W += - learning_rate * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -79,19 +85,22 @@ class LinearClassifier(object):
     data points.
 
     Inputs:
-    - X: D x N array of training data. Each column is a D-dimensional point.
+    - X: N x D array of training data. Each column is a D-dimensional point.
 
     Returns:
     - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
       array of length N, and each element is an integer giving the predicted
       class.
     """
-    y_pred = np.zeros(X.shape[1])
+    y_pred = np.zeros(X.shape[0])
     ###########################################################################
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    # Sort from lowest to high the scores of all the clases and put indices into array
+    # Then index that array so that you create an N x 1 matrix with the last column
+    y_pred = np.argsort(X.dot(self.W), axis=1)[:,-1]
+    print y_pred[:10]
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -29,7 +29,34 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  #Normalize the scores to prevent blowup
+  num_examples = X.shape[0]
+  num_classes = W.shape[1]
+  
+  scores -= np.matrix(np.max(scores, axis=1)).T
+  for example_index in xrange(X.shape[0]):
+    the_sum = 0.0
+    for class_index in xrange(W.shape[1]):
+      the_sum += np.exp(scores[example_index][class_index])
+    
+    loss += np.log(the_sum)  
+    loss -= scores[example_index][y[example_index]]
+
+    for class_index in xrange(W.shape[1]):
+      p =  np.exp(scores[example_index][class_index])/the_sum
+      if (class_index == y[example_index]):
+        dW[:, class_index] += (p-1) * X[example_index]
+      else:
+        dW[:, class_index] += p*X[example_index]
+
+  #Average
+  loss /= num_examples 
+  dW /= num_examples 
+
+  #Regularization
+  loss += reg * np.sum(W * W)
+  dW += 2 * reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +80,39 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  #Scores Shape: N x C
+  scores = X.dot(W)
+  scores -= np.matrix(np.max(scores, axis=1)).T
+
+  #Sums Shape: 1 x N  
+  sums = np.matrix(np.sum(np.exp(scores), axis=1))
+
+
+  n = X.shape[0]
+  c = W.shape[1]
+
+  #Probs Shape: N x C
+  probs = np.exp(scores) / sums.T
+  correct_inds = np.zeros(probs.shape)
+  correct_inds[np.arange(n), y] = 1
+  probs -= correct_inds
+
+  #Add in the sums to the loss
+  loss += np.sum(np.log(sums))
+
+  #Add in the numerator of the orignal log equation
+  loss -= np.sum(scores[np.arange(len(scores)), y])
+
+  #dW Shape: D x C
+  dW += X.T.dot(probs)
+
+
+
+  
+  #Normalize
+  loss /= n
+  dW   /= n
+  
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
